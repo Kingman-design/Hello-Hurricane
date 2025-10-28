ecedd1a0ef9e1d8cd165267b8b0bf22afd6247d6
timeportal2000 <timeportal2000@gmail.com>
2025-10-28 16:19:43 -0400

    added player movement

---
+++ team Hello-Hurricane/Assets/scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int speed;
+    [SerializeField] int JumpSpeed;
+    [SerializeField] int maxJumps;
+    [SerializeField] int gravity;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    int jumpCount;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();  
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            playerVel = Vector3.zero;
+            jumpCount = 0;
+        }
+        else
+        {
+            playerVel.y -= gravity * Time.deltaTime;
+        }
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+        controller.Move(playerVel * Time.deltaTime);
+    }
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
+        {
+            playerVel.y = JumpSpeed;
+            jumpCount++;
+        }
+    }
+}
