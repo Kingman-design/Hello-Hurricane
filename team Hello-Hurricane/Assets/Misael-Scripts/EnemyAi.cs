d08aa315f471fa83fd73be2301e534e14c4ffb75
bros4lifemichatavo@gmail.com <bros4lifemichatavo@gmail.com>
2025-11-02 18:14:35 -0500

    Enemy Jump
    Enemy now is able to jump over jumpable obstacles and added a collider to not phase pass through walls

--- team Hello-Hurricane/Assets/Misael-Scripts/EnemyAi.cs
+++ team Hello-Hurricane/Assets/Misael-Scripts/EnemyAi.cs
@@ -1,21 +1,74 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAi : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] int gravity;
+    [SerializeField] int jumpspeed;
+    [SerializeField] int speed;
+    [SerializeField] Rigidbody rb;
+    //[SerializeField] NavMeshAgent agent;
 
-    int speed = 3;
+    bool ObstacleTrigger;
+    Vector3 EnemyVel;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player-MisealTest").GetComponent<Transform>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        enemyMovement();
+
+        if(ObstacleTrigger)
+        {
+            DodgeJump();
+        }
+        else
+        {
+            Vector3 vel = rb.linearVelocity;
+            vel.y -= gravity * Time.deltaTime;
+            rb.linearVelocity = vel;
+        }
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("JumpMG"))
+        {
+            ObstacleTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("JumpMG"))
+        {
+            ObstacleTrigger = false;
+        }
+    }
+
+    void enemyMovement()
+    {
+        Vector3 moveDir = (target.position - transform.position).normalized;
+        moveDir.y = 0;
+
+        Vector3 moveVel = moveDir * speed;
+
+        rb.linearVelocity = moveVel;
+    }
+
+    void DodgeJump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpspeed, rb.linearVelocity.z);
+    }
+
 }
