22a753a65e8cd017a95b1633a1d7cfccd645a0fe
Renee-Ench <RBEnchautegui@student.fullsail.edu>
2025-11-06 04:42:58 -0500

    start and update void

--- team Hello-Hurricane/Assets/Renee - Audio/Audio.cs
+++ team Hello-Hurricane/Assets/Renee - Audio/Audio.cs
@@ -12,7 +12,32 @@ public class Audio : MonoBehaviour
     [SerializeField] float pitchRate = 0.05f;
     [SerializeField] float maxPitch = 2.5f;
 
-    
+    bool wasGroundLastF; 
+    float bAudioPitch;
+    float bRunPitch;
+
+    void Start()
+    {
+        bAudioPitch = bgEnviroAudio.pitch;  //starting pitch for the loops
+        bRunPitch = runAudio.pitch;
+
+        if(bgEnviroAudio != null && !bgEnviroAudio.isPlaying) // plays bg loop
+        {
+            bgEnviroAudio.Play();
+
+            wasGroundLastF = controller.isGrounded; 
+        } 
+    }
+
+    void Update()
+    {
+        hdlrRun();
+        hdlrJump();
+        hdlrLand();
+        hdlrDuck();
+        incPitchOverTime();
+
+    }
 
 
 }
